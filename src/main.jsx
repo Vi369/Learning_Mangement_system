ec1361ce3c16efe9dff780ff0cc1ff650f92fb89
vi369 <vikashverma3001@gmai.com>
2023-12-02 14:38:20 +0530

    resole

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,20 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
+import store from './redux/Store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  
     <React.StrictMode>
-      <App />
-      <Toaster/>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Toaster/>
+        </BrowserRouter>
+      </Provider>
     </React.StrictMode>,
-  </BrowserRouter>
+  
 )
